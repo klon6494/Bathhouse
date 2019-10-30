03035fb0bf82f572f1c58b127c3e62a9ce96300c
Nikita <klon6494@gmail.com>
2019-10-30 11:34:12 +0400

    Поправлена проблема с переполнением памяти

--- app/src/main/java/com/example/bathhouse/MyApplication.java
+++ app/src/main/java/com/example/bathhouse/MyApplication.java
@@ -5,10 +5,25 @@ import android.content.Context;
 import android.database.SQLException;
 
 import java.io.IOException;
+import java.util.Stack;
 
 public class MyApplication extends Application {
 
     private DataBaseHelper m_db;
+    private Stack<Integer> backStack = new Stack<>();
+
+    public void pushStack(int id)
+    {
+        backStack.push(id);
+    }
+
+    public Integer popStack()
+    {
+        if(backStack.size() > 0)
+            return backStack.pop();
+        else
+            return -1;
+    }
 
     public void initializeDb(Context context) {
         m_db = new DataBaseHelper(context);

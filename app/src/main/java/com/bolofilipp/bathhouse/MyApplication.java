9c70afa643967ec80b9c0ceb3584ab1db9673e98
Nikita <klon6494@gmail.com>
2019-11-08 13:28:23 +0400

    Сортировка итемов

--- app/src/main/java/com/bolofilipp/bathhouse/MyApplication.java
+++ app/src/main/java/com/bolofilipp/bathhouse/MyApplication.java
@@ -87,6 +87,20 @@ public class MyApplication extends Application {
                 list.add(item);
             }
         }
+
+        for(int i = 0; i < list.size() - 1; i++)
+        {
+            for(int j = 0; j < list.size() - i - 1; j++)
+            {
+                if(list.get(j).id > list.get(j+1).id)
+                {
+                    DBItem tmp = list.get(j);
+                    list.set(j, list.get(j+1));
+                    list.set(j+1, tmp);
+                }
+            }
+        }
+
         return list;
     }
 
